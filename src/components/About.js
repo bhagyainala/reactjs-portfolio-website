de6f7fac20bd99e6b63703992d6cbd55e84e6551
bhagyainala <156509930+bhagyainala@users.noreply.github.com>
2024-01-15 14:48:58 +0530

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { Icon } from "@iconify/react";
+import angularIcon from "@iconify/icons-logos/angular-icon";
+import reactIcon from "@iconify/icons-logos/react";
+import javaScriptIcon from "@iconify/icons-logos/vue";
+
+class About extends Component {
+  render() {
+    if (this.props.sharedBasicInfo) {
+      var profilepic = "images/" + this.props.sharedBasicInfo.image;
+    }
+    if (this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.about;
+      var hello = this.props.resumeBasicInfo.description_header;
+      var about = this.props.resumeBasicInfo.description;
+    }
+
+    return (
+      <section id="about">
+        <div className="col-md-12">
+          <h1 style={{ color: "black" }}>
+            <span>{sectionName}</span>
+          </h1>
+          <div className="row center mx-auto mb-5">
+            <div className="col-md-4 mb-5 center">
+              <div className="polaroid">
+                <span style={{ cursor: "auto" }}>
+                  <img
+                    height="250px"
+                    src="https://media.licdn.com/dms/image/D4E03AQGDn2fqDlEoaw/profile-displayphoto-shrink_800_800/0/1701436178274?e=1710979200&v=beta&t=-cQrkOBrEkaBUyyrKT3PfqL-Og6gW12pinIQ1aM0wB8"
+                    alt="Avatar placeholder"
+                  />
+                    <Icon
+                    icon={reactIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                  <Icon
+                    icon={angularIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                
+             
+                </span>
+              </div>
+            </div>
+
+            <div className="col-md-8 center">
+              <div className="col-md-10">
+                <div className="card">
+                  <div className="card-header">
+                    <span
+                      className="iconify"
+                      data-icon="emojione:red-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:yellow-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:green-circle"
+                      data-inline="false"
+                    ></span>
+                  </div>
+                  <div
+                    className="card-body font-trebuchet text-justify ml-3 mr-3"
+                    style={{
+                      height: "auto",
+                      fontSize: "132%",
+                      lineHeight: "200%",
+                    }}
+                  >
+                    <br />
+                    <span className="wave">{hello} :) </span>
+                    <br />
+                    <br />
+                    {about}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Education.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import {
+    VerticalTimeline,
+    VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import Badge from "react-bootstrap/Badge";
+
+class Education extends Component {
+    render() {
+
+
+        return (
+            <section id="education" className="pb-5">
+                <div className="col-md-12 mx-auto">
+                    <div className="col-md-12">
+                        <h1 className="section-title" style={{ color: "black" }}>
+                            <span className="text-black" style={{ textAlign: "center" }}>
+                                Education
+                            </span>
+                        </h1>
+                    </div>
+                </div>
+                <div className="col-md-8 mx-auto">
+                    <VerticalTimeline>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+
+                            iconStyle={{
+                                background: "#AE944F",
+                                color: "#fff",
+                                textAlign: "center",
+                            }}
+                            icon={<i className="fab fa-angular experience-icon"></i>}
+
+                        >
+
+                            <h4
+                                className="vertical-timeline-element-subtitle"
+                                style={{ textAlign: "left" }}
+                            >
+                            Sir C.R.R. College Of Engineerng
+                            </h4>
+
+                            <h3
+                                className=""
+                                style={{}}
+                            >
+                                <p> B.Tech(2016-2020)</p>
+                            </h3>
+
+                        </VerticalTimelineElement>
+
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+
+                            iconStyle={{
+                                background: "#AE944F",
+                                color: "#fff",
+                                textAlign: "center",
+                            }}
+                            icon={<i className="fab fa-angular experience-icon"></i>}
+
+                        >
+
+                            <h4
+                                className="vertical-timeline-element-subtitle"
+                                style={{ textAlign: "left" }}
+                            >
+                              Northern Arizona University
+
+                            </h4>
+
+                            <h3
+                                className=""
+                                style={{}}
+                            >
+                                <p>Master's degree, Computer ScienceMaster's degree, Computer Science</p>
+                            </h3>
+
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            iconStyle={{
+                                background: "#AE944F",
+                                color: "#fff",
+                                textAlign: "center",
+                            }}
+                            icon={
+                                <i className="fas fa-hourglass-start mx-auto experience-icon"></i>
+                            }
+                        />
+
+                    </VerticalTimeline>
+
+                </div>
+
+            </section>
+        );
+    }
+}
+
+export default Education;
